d9b1abdade15bbae6096852d194df3550f0d7635
Rabin Nankhwa <rabin.nankhwa@gmail.com>
2021-06-14 16:29:31 +0545

    separate volumeCredits loop

--- statement.js
+++ statement.js
@@ -3,12 +3,15 @@ function statement (invoice, plays) {
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`; 
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf)
 
         // print line for this order
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience}`
         totalAmount += amountFor(perf);
     }
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf)
+    }
+
     result += `Amount owed is ${usd(totalAmount/100)}\n`; 
     result += `You earned ${volumeCredits} credits\n`; 
     return result;
