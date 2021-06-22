a7395ad4527769ec098189c5320508781006c9e2
Rabin Nankhwa <rabin.nankhwa@gmail.com>
2021-06-22 17:53:50 +0545

    move comedy amount calculation

--- createStatementData.js
+++ createStatementData.js
@@ -49,22 +49,7 @@ class PerformanceCalculator {
 
 
     get amount() {
-        let amount = 0;
-
-        switch (this.play.type) {
-            case "tragedy":
-                throw new Error("subclass responsibility")
-            case "comedy":
-                amount = 30000;
-                if (this.performance.audience > 20) {
-                    amount += 10000 + 500 * (this.performance.audience - 20);
-                }
-                amount += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${this.play.type}`);
-        }
-        return amount;
+        throw new Error("subclass responsibility");
     }
 
     get volumeCredits() {
@@ -86,6 +71,14 @@ class TragedyCalculator extends PerformanceCalculator {
 }
 
 class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let amount = 30000;
+        if (this.performance.audience > 20) {
+            amount += 10000 + 500 * (this.performance.audience - 20);
+        }
+        amount += 300 * this.performance.audience;
+        return amount;
+    }
 }
 
 module.exports = {
